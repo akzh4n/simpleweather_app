234b3b84bdc9994b7cf6369ea3c334307e01fe85
manste1n <akzhan.kaz2003@gmail.com>
2022-08-14 03:34:56 +0600

    Initial Commit

---
+++ simple_weather_project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simple_weather_project
+//
+//  Created by Акжан Калиматов on 14.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
